2dcd6d3d2d6f5262cf1b5d8944952440f62bc628
Mateo Revilla <mateorevillalecca@gmail.com>
2020-11-25 13:30:31 -0500

    Added Feliks update

--- postgre.js
+++ postgre.js
@@ -1,35 +1,35 @@
-const {Pool} = require('pg')
+const { Pool } = require('pg')
 
 
 //-----------------------------CONFIGURE---------------------------------------
 
 const pool = new Pool({
-    user: 'master',
-    host: 'database.c8ib7ubvtm2x.us-east-2.rds.amazonaws.com',
-    database: 'pokerDatabase',
-    password: 'master12345',
-    port: 5432,
+  user: 'master',
+  host: 'database.c8ib7ubvtm2x.us-east-2.rds.amazonaws.com',
+  database: 'pokerDatabase',
+  password: 'master12345',
+  port: 5432,
+})
+
+//-----------------------------EXAMPLE & TEST---------------------------------------
+
+function example() {//RETURNS ALL THE TABLE IN REGISTERS
+  pool.query('SELECT * FROM register', (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(res.rows)
+    }
+    pool.end()
   })
+}
 
-  //-----------------------------EXAMPLE & TEST---------------------------------------
-
-  function example() {//RETURNS ALL THE TABLE IN REGISTERS
-    pool.query('SELECT * FROM register', (err, res) => {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log(res.rows)
-        }
-        pool.end()
-      })
-  }
-
-  function test() {//ADD HERE WHATAVER YOU WANT TO TEST
-    example()
-  }
+function test() {//ADD HERE WHATAVER YOU WANT TO TEST
+  example()
+}
 
 
-  //-----------------------------REGISTER TABLE--------------------------------------
+//-----------------------------REGISTER TABLE--------------------------------------
 
   //REGISTER NEW TABLE (MATEO)
   function registerNewTable(small_blind, big_blind, number_participants,
@@ -59,7 +59,92 @@ const pool = new Pool({
 
   //Add New participants (FELIKS)
 
-  function addNewParticipant() {
+  function addNewParticipant(username, table_id) {
+    //  const registerQuery = {
+    //      text: 'insert into register(start_time) values($1) returning table_id',
+    //      values: []
+   //   }
+      let max = "0"
+      //'SELECT DISTINCT max_number_participants FROM register'
+      pool.query('SELECT DISTINCT max_number_participants FROM register', (errMax, resMax) => {
+          if (errMax)
+          {
+              console.log(errMax)
+              return null
+          } else
+              {
+              console.log(resMax.rows[0])
+              max = resMax.rows[0].max_number_participants
+          }
+
+      })
+
+
+      if (max < Number("8"))
+      {
+          const newParicipant = {
+              text: 'UPDATE register SET participants_usernames = append_array(participants_usernames, values($1)) WHERE table_id = values($2)',
+              values: [username, table_id]
+          }
+
+
+          pool.query(newParicipant, (errParicipant, resParicipant) => { //insert to register user
+              if (errParicipant)
+              {
+                  console.log(errParicipant)
+                  return null
+              }
+
+              else
+
+              {
+                  const table_row = resParicipant.rows[0]
+                  console.log(table_row)
+
+              }
+          })
+
+
+          const setMoney = {
+              text: 'UPDATE register SET paritcipants_current_money = append_array(paritcipants_current_money, values($1)) WHERE table_id = values($2)',
+              values: ["1000", table_id]
+          }
+
+          pool.query(setMoney, (errSetMoney, resSetMoney) => { //insert to register user
+              if (errSetMoney)
+              {
+                  console.log(errSetMoney)
+                  return null
+              }
+
+              else
+
+              {
+                  const table_row = resSetMoney.rows[0]
+                  console.log(table_row)
+
+              }
+          })
+
+
+
+
+
+
+      }
+    /*
+      pool.query('SELECT max_number_participants FROM register WHERE table_id = values($1)', (err, res) => {
+          if (err)
+          {
+              console.log(err)
+          } else
+          {
+              console.log(res.rows)
+          }
+          pool.end()
+      }) */
+
+
 
   }
 
@@ -67,6 +152,30 @@ const pool = new Pool({
 
   function getInfoForTableID(table_id) {
     /*return the information of this table*/
+      const seeTableIdQuery = {
+          text: 'SELECT * FROM register WHERE table_id = values($1)',
+          values: [table_id]
+      }
+
+
+      pool.query(seeTableIdQuery, (errSeeTable, resSeeTable) => { //insert to register user
+          if (errSeeTable)
+          {
+              console.log(errSeeTable)
+              return null
+          }
+
+          else
+
+          {
+              const table_row = resSeeTable.rows[0]
+              console.log(resSeeTable.fields.map(field => field.name))
+              console.log(table_row)
+
+          }
+      })
+
+
   }
 
 
@@ -85,8 +194,53 @@ const pool = new Pool({
 
   //Update current_round_type (FELIKS)
 
-  function  updateCurrentRoundType(number){
+  function  updateCurrentRoundType(number, table_id){
     /*Update table_cards_active to number*/
+      const setRoundIdQuery = {
+          text: 'UPDATE register SET current_game_id = values($1) WHERE table_id = values($2)',
+          values: [number, table_id]
+      }
+
+
+      pool.query(setRoundIdQuery, (errSetRoundIdQuery, resSetRoundIdQuery) => { //insert to register user
+          if (errSetRoundIdQuery)
+          {
+              console.log(errSetRoundIdQuery)
+              return null
+          }
+
+          else
+
+          {
+              const table_row = resSetRoundIdQuery.rows[0]
+              console.log(table_row)
+
+          }
+      })
+
+
+      const setRoundIdQuery_gameTable = {
+          text: 'UPDATE game SET current_round_type = values($1) WHERE table_id = values($2)',
+          values: [number, table_id]
+      }
+
+
+      pool.query(setRoundIdQuery_gameTable, (err_SetRoundIdQuery_gameTable, res_SetRoundIdQuery_gameTable) => { //insert to register user
+          if (err_SetRoundIdQuery_gameTable)
+          {
+              console.log(err_SetRoundIdQuery_gameTable)
+              return null
+          }
+
+          else
+
+          {
+              const table_row = res_SetRoundIdQuery_gameTable.rows[0]
+              console.log(table_row)
+
+          }
+      })
+
   }
 
 
@@ -97,6 +251,28 @@ const pool = new Pool({
 
 //UPdate game player turn, active participants, matching bet, participantsbets, current_action_id (FELIKS)
 function newAction(game_id, player, action_type, amount) {
+    const set_newAction = {
+        text: 'INSERT INTO action(action_id, game_id, player, action_type, amount) values($1, $2, $3, $4, $5)',
+        values: ["12345", game_id, player, action_type, amount]
+    }
+
+    pool.query(set_newAction, (err_set_newAction, res_set_newAction) => { //insert to register user
+        if (err_set_newAction)
+        {
+            console.log(err_set_newAction)
+            return null
+        }
+
+        else
+
+        {
+            const table_row = res_set_newAction.rows[0]
+            console.log(table_row)
+
+        }
+    })
+
+    
 
 }
 
